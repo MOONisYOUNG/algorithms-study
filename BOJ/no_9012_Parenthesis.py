d48126dc5490ab91db67a8d4b495e5b8884e8db3
MOONisYOUNG <bigsmy6954@gmail.com>
2023-03-29 15:19:22 +0900

    3th commit

---
+++ BOJ/no_9012_Parenthesis.py
@@ -0,0 +1,20 @@
+import sys
+N = int(input())
+
+def parenthesis(string):
+  stack = []
+  for s in string:
+    if s == '(':
+      stack.append(s)
+    else: # s == ')'일 경우
+      if not stack or stack[-1]  == ')':
+        return "NO"
+      stack.pop()
+  if stack:
+    return "NO"
+  return "YES"
+
+  
+for _ in range(N):
+  string = sys.stdin.readline()
+  print(parenthesis(string[:-1]))
