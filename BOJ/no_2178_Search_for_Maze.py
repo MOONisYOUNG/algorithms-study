43406486cb46fcabc2d0da2fc98a80e9c27aeb2e
MOONisYOUNG <bigsmy6954@gmail.com>
2023-04-06 16:00:30 +0900

    19th commit

---
+++ BOJ/no_2178_Search_for_Maze.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input().rstrip())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0 ,-1, 1]
+
+def BFS(x, y):
+    queue = deque()
+    queue.append((x, y))
+    
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or ny < 0 or nx >= N or ny >= M:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    return graph[N-1][M-1]
+    
+    
+print(BFS(0, 0))
