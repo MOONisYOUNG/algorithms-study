c0f67519644a04f1be746b1422014f0ca0e47a06
MOONisYOUNG <bigsmy6954@gmail.com>
2023-04-10 11:47:06 +0900

    26th commit

---
+++ BOJ/no_1259_Palindrome_Number.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+def is_palindrome(string):
+    return string[::1] == string[::-1]
+
+while True:
+    num_string = input().rstrip()
+    
+    if num_string == '0':
+        break
+    
+    if is_palindrome(num_string):
+        print("yes")
+    else:
+        print("no")
