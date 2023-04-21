b64477a80a09b01adaac2b02de36b7b41b3ad2d2
MOONisYOUNG <bigsmy6954@gmail.com>
2023-04-21 14:54:15 +0900

    41th commit

--- BOJ/no_1912_Continuous_Sum.py
+++ BOJ/no_1912_Continuous_Sum.py
@@ -3,9 +3,11 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-dp = list(map(int, input().split()))
+num_li = list(map(int, input().split()))
 
+dp = [0] * n
+dp[0] = num_li[0]
 for idx in range(1, n):
-    dp[idx] = max(dp[idx], dp[idx-1] + dp[idx])
+    dp[idx] = max(num_li[idx], dp[idx-1] + num_li[idx])
     
 print(max(dp))
