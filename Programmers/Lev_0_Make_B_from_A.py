52315fce16afd5f517bcf7452e1e90195594176c
MoonYoung Song <87643414+MOONisYOUNG@users.noreply.github.com>
2025-01-04 22:29:29 +0900

    Delete Programmers directory

--- Programmers/Lev_0_Make_B_from_A.py
+++
@@ -1,7 +0,0 @@
-from collections import Counter
-def solution(before, after):
-      
-    if Counter(before) == Counter(after):
-        return 1
-    else:
-        return 0
--- Programmers/Lev_3.Find_Missing_Data.sql
+++
@@ -1,7 +0,0 @@
-# 1. ANIMAL_INS와 ANIMAL_OUTS에 ANIMAL_ID 컬럼 기준으로 right outer join를 적용
-# 2. ANIMAL_INS.DATETIME 정보가 없는 row들만 찾기
-# 3. ANIMAL_ID와 NAME만 출력하기
-
-SELECT B.ANIMAL_ID, B.NAME
-FROM ANIMAL_INS as A right outer join ANIMAL_OUTS as B on A.ANIMAL_ID = B.ANIMAL_ID
-WHERE A.DATETIME IS NULL
