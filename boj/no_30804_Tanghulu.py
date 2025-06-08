a4d58bd4b0abaf4ee0acf7e52a3eb969ec173a73
MoonYoung <bigsmy6954@gmail.com>
2025-06-08 14:08:15 +0900

    BOJ no.30804 (Brute Force, Two Pointers)
    - Using Language : Python

---
+++ boj/no_30804_Tanghulu.py
@@ -0,0 +1,31 @@
+import sys
+from typing import List
+from collections import defaultdict
+input = sys.stdin.readline
+
+n = int(input())
+fruits_li = list(map(int, input().split()))
+
+def solution(n: int, fruits_li: List[int]) -> int:
+    answer = 0
+    
+    left = 0
+    counts_dict = defaultdict(int)
+    
+    for right in range(n):
+        counts_dict[fruits_li[right]] += 1
+        
+        while len(counts_dict) > 2:
+            left_num = fruits_li[left]
+            counts_dict[left_num] -= 1
+            
+            if counts_dict[left_num] == 0:
+                del counts_dict[left_num]
+                
+            left += 1
+            
+        answer = max(answer, right - left + 1)
+        
+    return answer
+    
+print(solution(n, fruits_li))
