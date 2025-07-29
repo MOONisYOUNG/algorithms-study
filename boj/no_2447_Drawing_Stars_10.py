acf9d4042658ba10cb754f2b0c6b00cf4e6a3a87
MoonYoung <bigsmy6954@gmail.com>
2025-07-29 21:50:25 +0900

    BOJ no.2447 (Divide-and-Conquer, Recursion)
    - Code Refactoring

--- boj/no_2447_Drawing_Stars_10.py
+++ boj/no_2447_Drawing_Stars_10.py
@@ -1,23 +1,23 @@
-# tag - 분할 정복, 재귀
-def draw_stars(n):
-    if n==1:
+from typing import List
+
+def solution(n: int) -> List[int]:
+    if n == 1:
         return ['*']
         
-    Stars=draw_stars(n//3)
-    L=[]
-    
-    # 리스트에 담긴 내용물을 str 형태로 풀어주기 위해서 for 반복문 사용
-    for star in Stars: 
-        L.append(star*3)
-        
-    for star in Stars:
-        L.append(star+' '*(n//3)+star)
-        
-    for star in Stars:
-        L.append(star*3)
-        
-    return L
+    stars = solution(n // 3)
+    star_li = []
 
+    for star in stars: 
+        star_li.append(star * 3)
+        
+    for star in stars:
+        star_li.append(star + (' '  * (n//3)) + star)
+        
+    for star in stars:
+        star_li.append(star * 3)
+        
+    return star_li
 
-N=int(input())
-print('\n'.join(draw_stars(N)))
+if __name__ == "__main__":
+    n = int(input())
+    print(*solution(n), sep='\n')
